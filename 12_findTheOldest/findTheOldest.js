623b1a5ca485f6b94425071b406ded70da3fbb4a
argon-3467 <akhildhatterwal@gmail.com>
2023-08-05 15:38:18 +0530

    Add solution of 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,35 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  // let currYear = new Date().getFullYear();
+  // let oldestPerson,
+  //   oldestPersonAge = 0;
+  // arr.forEach((person) => {
+  //   let age;
+  //   if (person.yearOfDeath) age = person.yearOfDeath - person.yearOfBirth;
+  //   else age = currYear - person.yearOfBirth;
+  //   if (age > oldestPersonAge) {
+  //     oldestPersonAge = age;
+  //     oldestPerson = person;
+  //   }
+  // });
+  // return oldestPerson;
 
+  //method 2
+  return arr.reduce((oldest, currentPerson) => {
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    const currentAge = getAge(
+      currentPerson.yearOfBirth,
+      currentPerson.yearOfDeath
+    );
+    return oldestAge < currentAge ? currentPerson : oldest;
+  });
+
+};
+
+const getAge = function (birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
 };
 
 // Do not edit below this line
