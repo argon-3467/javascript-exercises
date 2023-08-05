af7104228146b4ed80ff8fc132c8e1435ef4d9a0
argon-3467 <akhildhatterwal@gmail.com>
2023-08-05 14:31:16 +0530

    Add solution of 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let newstr = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+  return newstr.split("").reverse().join("") == newstr;
 };
 
 // Do not edit below this line
