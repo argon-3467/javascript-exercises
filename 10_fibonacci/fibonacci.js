e536ee0f5eeb91b73864ebfc20dac893681eac96
argon-3467 <akhildhatterwal@gmail.com>
2023-08-05 14:39:36 +0530

    Add solution of 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(term) {
+  let an_1 = 0;
+  let an = 1;
+  if (term < 0) return "OOPS";
+  else if (term == 0) return an_1;
+  else if (term == 1) return an;
+  else
+    for (let i = 2; i <= term; i++) {
+      [an, an_1] = [an + an_1, an];
+    }
+  return an;
 };
 
 // Do not edit below this line
